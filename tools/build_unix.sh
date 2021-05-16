2cd172ef041478903d39c2132d07870ecb950304
Ruben Perez <rubenperez038@gmail.com>
2021-05-16 11:29:48 +0200

    Attempt to fix build problems

--- tools/build_unix.sh
+++ tools/build_unix.sh
@@ -146,7 +146,6 @@ function cmake_build {
 }
 
 # Main script
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 setup_db
 
 if [ "$B2_TOOLSET" != "" ]; then # Boost.Build
